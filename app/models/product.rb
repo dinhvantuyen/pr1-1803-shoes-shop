c4bedbbbaddaf9068e114602d7fee9e6fe8cb4f7
dinhvantuyen <dinhtuyen3795@gmail.com>
2018-11-29 20:23:38 +0700

    create product

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,10 @@ class Product < ApplicationRecord
   has_many :product_orders
   has_many :rates
   belongs_to :category
+  default_scope -> { order(created_at: :desc) }
+
+  mount_uploader :image, PictureUploader
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true
+  validates :description, presence: true
 end
